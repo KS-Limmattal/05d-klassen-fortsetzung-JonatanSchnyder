2173637339521c08314381dead72317264af93aa
Jonatan Giuseppe Schnyder <jonatan@shade>
2024-03-13 16:46:18 +0100

    Finished Deck.java + reformat, Code is not tested

--- Deck.java
+++ Deck.java
@@ -1,6 +1,5 @@
 import java.util.Random;
 import java.util.Arrays;
-import java.lang.*;
 
 /**
  * Diese Klasse repräsentiert einen Kartenstapel mit einer variablen Anzahl
@@ -27,6 +26,7 @@ import java.lang.*;
  */
 public class Deck {
 	private Card[] cards;
+	static Random rnd = new Random(); // for increased Randomnes in total
 
 	public Deck(Card[] cards) {
 		this.cards = cards;
@@ -38,7 +38,8 @@ public class Deck {
 		int counter = 0;
 		for (Suit suit: Suit.values()){
 			for (Rank rank: Rank.values()) {
-				// this.cards[counter].suit = suit; // this code is private
+				// this.cards[counter].suit = suit; // this 
+				// 	code is private
 				// this.cards[counter].rank = rank;
 				this.cards[counter] = new Card(suit ,rank );
 				counter++;
@@ -50,20 +51,39 @@ public class Deck {
 		return this.cards;
 	}
 
-	public void addCard(Card othcard) {
+	public void addCard(Card card) {
 		for (Card mycard: this.cards) {
-			if (mycard.equals(othcard)) {
-				System.out.println("This card already exists in the Deck");
+			if (mycard.equals(card)) {
+				System.out.println("This card is in the Deck");
 				return;
 			}
 		}
-		Card []newcard = new Card[this.cards.length + 1];
-		System.arraycopy(this.cards, 0, newcard, 0, this.cards.length + 1);
-		this.cards = newcard;
-		this.cards[this.cards.length - 1] = othcard;
-
-		return;
+		this.cards = Arrays.copyOf(this.cards, this.cards.length + 1);
+		this.cards[this.cards.length-1] = card;
 	}
+
+	public Card pop() {
+		Card out = this.cards[this.cards.length-1];
+		this.cards = Arrays.copyOf(this.cards, this.cards.length-1);
+		return out; // clone could work too?, check before next exam
+	}
+
+	public void shuffle() {
+		Card temp;
+		for (int i = 0; i < this.cards.length; i++) {
+			for (int j = i + 1; j < this.cards.length; j++) {
+				// for (int j = i;.. 
+				// wäre mein favorit um zwei Karten mit sich
+				// selbst zu tauschen
+				if (rnd.nextInt(1) < 1) {
+					temp = this.cards[i];
+					this.cards[i] = this.cards[j];
+					this.cards[j] = temp;
+				}
+			}
+		}
+	}
+
 
 
     
